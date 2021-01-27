1936433b481398cd18947cba30fb63a6618421f0
z-w123 <45365618+z-w123@users.noreply.github.com>
2021-01-27 11:11:34 +0000

    including 'sample capture status' field to script
    removing 'sample capture status' field from user metadata spreadsheet but injecting it back in using this script

--- trimming_metadata_spreadsheet.py
+++ trimming_metadata_spreadsheet.py
@@ -14,23 +14,25 @@ files_xlsx = glob.glob("*.xlsx") #should we accept other spreadsheet extensions?
 for f in files_xlsx:
     if fnmatch.fnmatch(f, '*genome*'):
         print('you are using an assembly spreadsheet')
-        df = pd.read_excel(f, usecols="L:AX", header=1, sheet_name='Sheet1') #col range suits v4 
+        df = pd.read_excel(f, usecols="L:AV", header=1, sheet_name='Sheet1') #col range suits v4 
         df = df.iloc[3:,]
         df.dropna(axis=0, how='all', inplace=True)
         df.insert(7,"submission_tool",'drag and drop uploader tool',allow_duplicates=True) #study #to inject constant into trimmed df
         df.insert(24,"submission_tool",'drag and drop uploader tool',allow_duplicates=True) #sample
+        df.insert(26,"sample capture status",'active surveillance in response to outbreak',allow_duplicates=False)
         print('user sample & study data below:')
         print(df)
-        df.to_csv('trimmed_assembly_study_sample_metadata.csv', index=False)
+        df.to_csv('trimmed_assembly_study_sample_metadata_27_jan.csv', index=False)
     elif fnmatch.fnmatch(f, '*raw_reads*'):
         print('you are using a raw reads spreadsheet')
-        df = pd.read_excel(f, usecols="B:AM", header=1, sheet_name='Sheet1') #col range suits v4
+        df = pd.read_excel(f, usecols="B:AL", header=1, sheet_name='Sheet1') #col range suits v4
         df = df.iloc[3:,]
         df.dropna(axis=0, how='all', inplace=True)
         df.insert(7,"submission_tool",'drag and drop uploader tool',allow_duplicates=True) #study #to inject constant into trimmed df
         df.insert(24,"submission_tool",'drag and drop uploader tool',allow_duplicates=True) #sample
+        df.insert(26,"sample capture status",'active surveillance in response to outbreak',allow_duplicates=False)
         print('user sample & study data below:')
         print(df)
-        df.to_csv('trimmed_raw_reads_study_sample_metadata.csv', index=False)
+        df.to_csv('trimmed_raw_reads_study_sample_metadata_27_jan.csv', index=False)
     else:
         print('you have used an unsupported spreadsheet, please try again')
